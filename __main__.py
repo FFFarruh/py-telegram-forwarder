20b80aa9da476733fc7da45f0e30b477cd99de19
FFFarruh <fffarruh@mail.ru>
2021-10-10 17:26:08 +0300

    Add help command

--- __main__.py
+++ __main__.py
@@ -7,15 +7,29 @@ setting = Settings()
 client = Client("", setting.api_id, setting.api_hash)
 
 
+async def help_command():
+    await client.send_message(
+        setting.target_id, "/help - Output a list of available commands.\n"
+    )
+
+
 @client.on_message()
 async def echo_message(client: Client, message: Message) -> None:
     global setting
-    if message.chat.id == setting.target_id:
+
+    if message.text == "/help" and message.chat.id == setting.target_id:
+        logging.info("Run command help: Message from target chat")
+        await help_command()
+        return
+
+    elif message.chat.id == setting.target_id:
         logging.info("Message echo ignored: Message from target chat")
         return
+
     elif message.chat.id not in setting.from_chat_ids:
         logging.info("Message echo ignored: This chat missing in white-list")
         return
+
     await client.forward_messages(
         setting.target_id, message.chat.id, message.message_id
     )
